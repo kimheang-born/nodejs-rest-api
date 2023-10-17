ba7647b83b581d6af87fe8ea012b8f9e4c4613e0
Kimheang <kimheang135@gmail.com>
2023-10-17 15:41:25 +0700

    chore: add pagination query

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,12 +5,23 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
   Post.find()
-    .sort({ createdAt: 'desc' })
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return Post.find()
+        .sort({ createdAt: 'desc' })
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
       res.status(200).json({
         message: 'Fetched posts successfully.',
         posts,
+        totalItems,
       });
     })
     .catch((err) => {
