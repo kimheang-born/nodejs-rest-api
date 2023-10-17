051f9392574c9a02ff6b4822852a3f8445458c05
Kimheang <kimheang135@gmail.com>
2023-10-17 17:34:06 +0700

    chore: implement login with JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.JWT_SECRET;
 
 const User = require('../models/user');
 
@@ -27,6 +29,50 @@ exports.signup = (req, res, next) => {
       res.status(201).json({
         message: 'User created!',
         userId: result._id,
+      });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  let loadedUser;
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        const error = new Error('A user with this email could not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+
+      const userId = loadedUser._id.toString();
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId,
+        },
+        SECRET_KEY,
+        {
+          expiresIn: '1h',
+        }
+      );
+      res.status(200).json({
+        token,
+        userId,
       });
     })
     .catch((err) => {
