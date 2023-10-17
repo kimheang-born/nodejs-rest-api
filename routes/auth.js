78d0e251c8fb1837a4d702b12f66cbd0b224ec46
Kimheang <kimheang135@gmail.com>
2023-10-17 16:38:37 +0700

    chore: implement user registration

---
+++ routes/auth.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { body } = require('express-validator');
+
+const User = require('../models/user');
+const authController = require('../controllers/auth');
+
+const router = express.Router();
+
+router.put(
+  '/signup',
+  [
+    body('email')
+      .isEmail()
+      .withMessage('Please enter a valid email.')
+      .custom((value, { req }) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+          if (userDoc) {
+            const error = new Error(
+              'E-Mail exists already, please pick a different one.'
+            );
+            error.statusCode = 422;
+            throw error;
+          }
+        });
+      })
+      .normalizeEmail(),
+    body('password').trim().isLength({ min: 5 }),
+    body('name').trim().not().isEmpty(),
+  ],
+  authController.signup
+);
+
+module.exports = router;
