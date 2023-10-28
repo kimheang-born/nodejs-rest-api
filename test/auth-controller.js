4c7ef014ceec02ac7ebf5abb2047d74166581f4c
Kimheang <kimheang135@gmail.com>
2023-10-28 15:51:57 +0700

    test: add unit test

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,10 +1,31 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const User = require('../models/user');
 const AuthController = require('../controllers/auth');
 
-describe('Auth Controller - Login', () => {
+describe('Auth Controller', () => {
+  let createdUserId; // Variable to store the user ID created in the "before" hook
+
+  before(async () => {
+    await mongoose.connect(process.env.DATABASE_UNIT_TEST_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    const user = new User({
+      email: 'test@test.com',
+      password: 'tester',
+      name: 'Test',
+      posts: [],
+    });
+
+    const savedUser = await user.save();
+    createdUserId = savedUser._id; // Store the created user's ID
+  });
+
   it('should throw an error with code 500 if accessing the database fails', async () => {
     const findOneStub = sinon.stub(User, 'findOne');
     findOneStub.throws();
@@ -24,5 +45,30 @@ describe('Auth Controller - Login', () => {
     }
 
     findOneStub.restore();
+  });
+
+  it('should send a response with a valid user status for an existing user', async () => {
+    const req = { userId: createdUserId }; // Use the created user's ID
+    const res = {
+      statusCode: 500,
+      userStatus: null,
+      status(code) {
+        this.statusCode = code;
+        return this;
+      },
+      json(data) {
+        this.userStatus = data.status;
+      },
+    };
+
+    await AuthController.getUserStatus(req, res, () => {});
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.userStatus).to.equal('I am new!');
+  });
+
+  after(async () => {
+    await User.deleteMany();
+    await mongoose.disconnect();
   });
 });
