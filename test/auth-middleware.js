ce799f2a5139873adb9a2018c23510ce766de0cd
Kimheang <kimheang135@gmail.com>
2023-10-28 14:16:36 +0700

    test: add new test unit

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -2,13 +2,24 @@ const { expect } = require('chai');
 
 const authMiddleware = require('../middleware/is-auth');
 
-it('should throw an error if no authorization header is present', () => {
-  const req = {
-    get(headerName) {
-      return null;
-    },
-  };
-  expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
-    'Unauthorized.'
-  );
+describe('Auth middleware', () => {
+  it('should throw an error if no authorization header is present', () => {
+    const req = {
+      get(headerName) {
+        return null;
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw(
+      'Unauthorized.'
+    );
+  });
+
+  it('should throw an error if the authorization header is only one string', () => {
+    const req = {
+      get(headerName) {
+        return 'xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
 });
