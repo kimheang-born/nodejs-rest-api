eb6cc7bfe9ba470a5813b2926e8964cadf9aa3d1
Kimheang <kimheang135@gmail.com>
2023-10-28 14:42:07 +0700

    test: add new unit test

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,4 +1,6 @@
 const { expect } = require('chai');
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 const authMiddleware = require('../middleware/is-auth');
 
@@ -18,6 +20,32 @@ describe('Auth middleware', () => {
     const req = {
       get(headerName) {
         return 'xyz';
+      },
+    };
+    expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', () => {
+    const req = {
+      get(headerName) {
+        return 'Bearer asdadfadfasfdas';
+      },
+    };
+    sinon.stub(jwt, 'verify');
+    jwt.verify.returns({
+      userId: 'abc',
+    });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property('userId');
+    expect(req).to.have.property('userId', 'abc');
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
+  });
+
+  it('should throw an error if the token cannot be verified', () => {
+    const req = {
+      get(headerName) {
+        return 'Bearer xyz';
       },
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
